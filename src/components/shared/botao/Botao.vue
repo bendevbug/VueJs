4db788ee0f5473e327dbd6300b72fc92afb42edb
unknown <maysaraquel.ia@gmail.com>
2023-04-27 19:20:17 -0300

    Começando no VueJs

---
+++ src/components/shared/botao/Botao.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <button @click="disparaAcao()" class="button danger-button normal-button">{{ rotulo }}</button>
+
+</template>
+
+<script>
+
+    export default {
+
+        props: ['tipo', 'rotulo'],
+
+        methods: {
+
+            disparaAcao() {
+                if(confirm('Confirma operação?')) {
+                    this.$emit('botaoAtivado');
+                }
+            }
+        }
+    }
+
+</script>
+
+
+<style>
+
+    .button {
+        display: inline-block;
+        padding: 10px;
+        border-radius: 3px;
+        margin: 10px;
+        font-size: 1.2rem;
+    }
+    
+    .danger-button {
+        background-color: firebrick;
+        color: white;
+    }
+
+    .normal-button {
+        background-color: darkcyan;
+        color: white;
+    }
+
+</style>
